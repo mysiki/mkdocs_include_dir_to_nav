a8f1fcbc8e076693f2a1d5c374a533d094c9854b
mysiki <hoaxboy@wanadoo.fr>
2021-10-12 16:04:19 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mkdocs_include_dir_to_nav",
-    version="1.0.0",
+    version="1.1.0",
     description="A MkDocs plugin include all file in dir to navigation",
     long_description=long_description,
     long_description_content_type="text/markdown",
